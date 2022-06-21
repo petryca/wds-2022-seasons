4b2a5e995f6e4007136161163fd9579abd8cdaba
David Petryca <david@petryca.com>
2022-06-21 13:03:12 +0200

    class 2

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 (() => {
 
     
+    const spring = document.querySelector('#spring');
+
+    const springArticle = document.querySelector('#spring>article');
+
+    spring.addEventListener('click', event => {
+        springArticle.classList.toggle('inviewport');
+    });
+
 
 })();
