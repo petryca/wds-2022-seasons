211ce2745d47a7009a5229092d4729670a1456b4
David Petryca <david@petryca.com>
2022-06-22 10:37:33 +0200

    finished

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 (() => {
 
     
-    const spring = document.querySelector('#spring');
+    const observerOptions = {
+        rootMargin: '1000px 0px 0px 0px',
+        threshold: 0.7
+    };
 
-    const springArticle = document.querySelector('#spring>article');
-
-    spring.addEventListener('click', event => {
-        springArticle.classList.toggle('inviewport');
+    const observe = entries => entries.forEach(entry => {
+        entry.target.classList.toggle('inviewport', entry.isIntersecting);
     });
 
+    const obs = new IntersectionObserver(observe, observerOptions);
+    document.querySelectorAll('article').forEach(el => obs.observe(el));
+
+    
 
 })();
